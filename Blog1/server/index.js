05b00dfa923087d3dbb563a944ca72aee72551f0
ppalniraj1996 <97506350+ppalniraj1996@users.noreply.github.com>
2022-06-26 22:37:10 +0530

    done

--- Blog1/server/index.js
+++ Blog1/server/index.js
@@ -1,14 +1,18 @@
-const express =require("express");
-const connection =require("./MongoDb");
-const blogRouter =require("./Route/blogroutes");
+const express = require("express");
+const connection = require("./MongoDb");
+const blogRouter = require("./Route/blogroutes");
+const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use('/',blogRouter)
-app.use('/blogs',blogRouter)
+app.use("/", blogRouter);
+app.use("/blogs", blogRouter);
 
-app.listen(8080, async()=>{
-    await connection;
-    console.log("Server is Started at Port 8080");
-})
+app.listen(8080, async () => {
+  await connection;
+  console.log("Server is Started at Port 8080");
+});
